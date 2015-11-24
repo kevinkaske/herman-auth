7576b824df9b41c3bc55198b52533ab074796ad6
Kevin Kaske <kevinkaske@gmail.com>
2015-11-24 11:36:29 -0600

    Slight format change

--- libs/HermanAuth.php
+++ libs/HermanAuth.php
@@ -10,6 +10,7 @@ Class HermanAuth {
 
 	public function __construct(){
 		global $config;
+		
 		$db = new MysqliDb ($config['db_host'], $config['db_username'], $config['db_password'], $config['db_database']);
 		$this->db = $db;
 
